3e1118192cb04a6830590a966338b3168797ae8f
marian <trnka@savba.sk>
2024-05-03 13:19:01 +0200

    podpora IPTV.merge

--- default.py
+++ default.py
@@ -22,22 +22,90 @@
 import os
 sys.path.append( os.path.join ( os.path.dirname(__file__),'resources','lib') )
 import markiza
+from epg_sms import get_sms_epg
 import xbmcprovider,xbmcaddon
 import util
+from urllib.parse import urlencode, parse_qsl
+
+import json
+import socket
+
+class IPTVManager:
+    """Interface to IPTV Manager"""
+
+    def __init__(self, port, channels, epgdays=3, epgdaysback=1):
+        """Initialize IPTV Manager object"""
+        self.port = port
+        self.channels = channels
+        self.epgdays = epgdays
+        self.epgdaysback = epgdaysback 
+
+    def via_socket(func):
+        """Send the output of the wrapped function to socket"""
+
+        def send(self):
+            """Decorator to send over a socket"""
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(('127.0.0.1', self.port))
+            try:
+                sock.sendall(json.dumps(func(self)).encode())
+            finally:
+                sock.close()
+
+        return send
+
+    @via_socket
+    def send_channels(self):
+        """Return JSON-STREAMS formatted python datastructure to IPTV Manager"""
+        return dict(version=1, streams=self.channels)
+
+    @via_socket
+    def send_epg(self):
+        """Return JSON-EPG formatted python data structure to IPTV Manager"""
+        epg_data=get_sms_epg(self.epgdays,self.epgdaysback)
+        return dict(version=1, epg=epg_data)
+
 
 __scriptid__   = 'plugin.video.markiza.sk'
 __scriptname__ = 'markiza.sk'
 __addon__      = xbmcaddon.Addon(id=__scriptid__)
 __language__   = __addon__.getLocalizedString
 
-settings = {'downloads':__addon__.getSetting('downloads'),'quality':__addon__.getSetting('quality')}
-params = util.params()
+#import web_pdb; web_pdb.set_trace()
+settings = {'downloads':__addon__.getSetting('downloads'),'quality':__addon__.getSetting('quality'),'epgdays':__addon__.getSetting('epgdays')}
+
 
 provider = markiza.markizaContentProvider()
 
-class XBMCMarkizaContentProvider(xbmcprovider.XBMCMultiResolverContentProvider):
-    def render_default(self, item):
-        self.render_dir(item)
+if len (sys.argv)>1 and 'port' in sys.argv[2][1:]:
+    channels=[]
+    if 'iptv/channels' in sys.argv[0]: 
+        paramstring=sys.argv[2][1:]
+        params = dict(parse_qsl(paramstring))
+        port=int(params['port'])
+
+        result=provider.categories()
+        for item in result:
+            if item['type']!='video':
+                continue
+            item2=provider.resolve(item)
+            item['url']=item2[0]['url']
+            channels.append(dict(id=item['title'],name=item['title'],logo=item['img'],stream=item2[0]['url']))
+        
+        IPTVManager(port,channels).send_channels()
+        
+    if 'iptv/epg' in sys.argv[0]: 
+        paramstring=sys.argv[2][1:]
+        params = dict(parse_qsl(paramstring))
+        port=int(params['port'])
+        epgdays=int(settings['epgdays'])
+        IPTVManager(port,channels,epgdays).send_epg()        
+
+else:    
+    params = util.params()
+    class XBMCMarkizaContentProvider(xbmcprovider.XBMCMultiResolverContentProvider):
+        def render_default(self, item):
+            self.render_dir(item)
             
         
-XBMCMarkizaContentProvider(provider,settings,__addon__).run(params)
+    XBMCMarkizaContentProvider(provider,settings,__addon__).run(params)
